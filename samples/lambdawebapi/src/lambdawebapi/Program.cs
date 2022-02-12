ddd3878193674e7e2220264cbf4b30643c3f3864
IgnacioColmenares <66320936+IgnacioColmenares@users.noreply.github.com>
2022-02-12 14:03:10 -0500

    trying fixing minimal 2

--- samples/lambdawebapi/src/lambdawebapi/Program.cs
+++ samples/lambdawebapi/src/lambdawebapi/Program.cs
@@ -1,12 +1,51 @@
 using lambdawebapi;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+
+/*var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 startup.Configure(app, builder.Environment);
+
+app.Run();
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+// Register Lambda to replace Kestrel as the web server for the ASP.NET Core application.
+// If the application is not running in Lambda then this method will do nothing. 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/" , ()=>"Welcome to running ASP.NET Core on AWS Lambda (minimal api)");
+app.MapGet("/env" , ()=>"enviroment");
 
 app.Run();
--- samples/lambdawebapi/src/lambdawebapi/Startup.cs
+++ samples/lambdawebapi/src/lambdawebapi/Startup.cs
@@ -26,7 +26,7 @@ namespace lambdawebapi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(WebApplication app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -38,6 +38,7 @@ namespace lambdawebapi
             app.UseRouting();
 
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
